76a269b0af9f98435b03683f251ac69640bbc76a
Ilya5527 <kyzmin.ilya2014@ynadex.ru>
2022-08-31 14:14:11 +0300

    Исправленное задание

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,6 +1,5 @@
 package hiber.dao;
 
-import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,26 +11,30 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
-   @Override
-   public User getByCar(String model, int series) {
-      Car car = (Car) sessionFactory.getCurrentSession().createQuery("from Car where model = :param1 and series = :param2" ).setParameter("param1", model).setParameter("param2", series).getSingleResult();
-      User user = (User) sessionFactory.getCurrentSession().createQuery("from User where carDetail = :param1").setParameter("param1", car).getSingleResult();
-      return user;
-   }
+    @Override
+    public User getByCar(String model, int series) {
+        User user = (User) sessionFactory
+                .getCurrentSession()
+                .createQuery("from User where carDetail.model = :param1 and carDetail.series = :param2")
+                .setParameter("param1", model)
+                .setParameter("param2", series)
+                .getSingleResult();
+        return user;
+    }
 
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -6,81 +6,82 @@ import javax.persistence.*;
 @Table(name = "users")
 public class User {
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   @Column(name = "id")
-   private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
-   @Column(name = "name")
-   private String firstName;
+    @Column(name = "name")
+    private String firstName;
 
-   @Column(name = "last_name")
-   private String lastName;
+    @Column(name = "last_name")
+    private String lastName;
 
-   @Column(name = "email")
-   private String email;
+    @Column(name = "email")
+    private String email;
 
-   @OneToOne(cascade = CascadeType.ALL)
-   @JoinColumn(name = "car_id")
-   private Car carDetail;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "car_id")
+    private Car carDetail;
 
-   public User() {}
-   
-   public User(String firstName, String lastName, String email, Car carDetail) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.email = email;
-      this.carDetail = carDetail;
-   }
+    public User() {
+    }
 
-   public Long getId() {
-      return id;
-   }
+    public User(String firstName, String lastName, String email, Car carDetail) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.carDetail = carDetail;
+    }
 
-   public void setId(Long id) {
-      this.id = id;
-   }
+    public Long getId() {
+        return id;
+    }
 
-   public String getFirstName() {
-      return firstName;
-   }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
+    public String getFirstName() {
+        return firstName;
+    }
 
-   public String getLastName() {
-      return lastName;
-   }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
+    public String getLastName() {
+        return lastName;
+    }
 
-   public String getEmail() {
-      return email;
-   }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
+    public String getEmail() {
+        return email;
+    }
 
-   public Car getCar() {
-      return carDetail;
-   }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-   public void setCar(Car carDetail) {
-      this.carDetail = carDetail;
-   }
+    public Car getCar() {
+        return carDetail;
+    }
 
-   @Override
-   public String toString() {
-      return "User{" +
-              "id=" + id +
-              ", firstName='" + firstName + '\'' +
-              ", lastName='" + lastName + '\'' +
-              ", email='" + email + '\'' +
-              ", carDetail=" + carDetail +
-              '}';
-   }
+    public void setCar(Car carDetail) {
+        this.carDetail = carDetail;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", carDetail=" + carDetail +
+                '}';
+    }
 }
